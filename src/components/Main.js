44e0b0ceee53337ae8d9263c94bc8d0a360b6205
Karema <karemahamdy51@gmail.com>
2024-07-31 22:47:48 +0300

    style: add loader, error screen

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+function Main() {
+  return(
+    <>
+    <main className="main">
+    <h2>Welcome to The React Quiz!</h2>
+      <h3>15 questions to test your React mastery</h3>
+      <button className="btn btn btn-ui">lets Start</button>
+    </main>
+    </>
+  )
+}
+export default Main
