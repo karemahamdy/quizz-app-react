d1bd337a37a57494f814aefab310862928089b14
Karema <karemahamdy51@gmail.com>
2024-07-31 23:32:57 +0300

    add use reducer ti fetch data in questions

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,41 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  status: "loading"
+};
+
+function reducer(state, action) {
+  console.log(state, action);
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "error":
+      return { ...state, status: "dataFailed" };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => {
+        console.error(err);
+        dispatch({ type: "error" });
+      });
+  }, []);
+
   return (
     <div className="app">
-      <Header/>
-      <Main/>
+      <Header />
+      <Main />
     </div>
   );
 }
