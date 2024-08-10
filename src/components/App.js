9bd8a975ef35e240d308fed65758ae0522d49256
Karema <karemahamdy51@gmail.com>
2024-08-10 22:01:53 +0300

    ..

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
@@ -10,22 +13,22 @@ const initialState = {
 function reducer(state, action) {
   console.log(state, action);
   switch (action.type) {
-    case "dataRecieved":
+    case "dataReceived":
       return { ...state, questions: action.payload, status: "ready" };
     case "error":
-      return { ...state, status: "dataFailed" };
+      return { ...state, status: "error" };
     default:
       throw new Error("Unknown action");
   }
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
+    fetch("http://localhost:9000/questions")
       .then((res) => res.json())
-      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => {
         console.error(err);
         dispatch({ type: "error" });
@@ -35,7 +38,12 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <Main />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen/>
+        }
+      </Main>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,10 @@
-function Main() {
+
+
+function Main({Children}) {
   return(
     <>
     <main className="main">
-    <h2>Welcome to The React Quiz!</h2>
-      <h3>15 questions to test your React mastery</h3>
-      <button className="btn btn btn-ui">lets Start</button>
+    {Children}
     </main>
     </>
   )
